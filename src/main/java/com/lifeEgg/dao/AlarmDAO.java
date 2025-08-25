3fbbf1ac71660f46d64d49c25ccfb14660af9d81
ohjou <hyperclutter@gmail.com>
2025-08-26 08:31:53 +0900

    #Feat: 알람 서비스 추가
    ################
    # 알람 서비스 추가, 기능에 맞는 알람 dao, mapper 수정.
    기능 - 생성, 아이디 별 삭제, uuid별 삭제, 유저 아이디 별 알림 읽어오기, uuid별 알림 읽어오기

--- src/main/java/com/lifeEgg/dao/AlarmDAO.java
+++ src/main/java/com/lifeEgg/dao/AlarmDAO.java
@@ -1,5 +1,8 @@
 package com.lifeEgg.dao;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.lifeEgg.dto.AlarmDTO;
@@ -10,11 +13,14 @@ public interface AlarmDAO {
 	
 public void create(AlarmDTO alarm) throws Exception;
     
-    public AlarmDTO read(Long id) throws Exception;
     
-    public AlarmDTO readByUuid(String uuid) throws Exception;
+    public Optional<AlarmDTO> readByUuid(String uuid);
+    
+    public List<AlarmDTO> readByUserId(Long userId);
 
-    public void delete(Long id) throws Exception;
+    public void deleteById(Long id) throws Exception;
+    
+    public void deleteByUuid(String uuid) throws Exception;
 
 
 }
