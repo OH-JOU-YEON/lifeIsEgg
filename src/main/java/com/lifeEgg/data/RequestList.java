90c4cab9ce7456eb5adcbfca683213852101cab2
ohjou <hyperclutter@gmail.com>
2025-08-28 16:35:02 +0900

    #Feat: 알람, 포스트 페이징 추가
    ################
    # List<Dto> 형식으로 db에서 받아온 데이터를 Page에 담는 메서드 작성
    컨트롤러 단에서 pageable 주입 필수

---
+++ src/main/java/com/lifeEgg/data/RequestList.java
@@ -0,0 +1,15 @@
+package com.lifeEgg.data;
+
+import org.springframework.data.domain.Pageable;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data 
+public class RequestList<T> {
+	
+	private T data;
+	private Pageable pageable;
+
+}
